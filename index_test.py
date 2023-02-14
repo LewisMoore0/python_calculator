ff579d1ab3b3310221eb11f0c832a408ed05e00e
Lewis Moore <lewismooredev@gmail.com>
2023-02-14 16:54:03 +0000

    add second test

--- index_test.py
+++ index_test.py
@@ -1,7 +1,11 @@
 from unittest import TestCase
 from index import add
+from index import subtract
 
 class IndexTest(TestCase):
 
     def test_add(self):
         assert add(1, 1) == 2
+
+    def test_subtract(self):
+        assert subtract(3, 1) == 2
